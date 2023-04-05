86915603fb20fb9cc2e999863aa1b19661425685
mkk <mk1210k@gmail.com>
2023-04-05 14:08:23 +0900

    [feat] add forminput component

---
+++ src/component/formInput.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+function FormInput({inputId, type, label, onChange, name, required, value, className, placeholder, disabled}){
+  return(
+    <Input className={inputId}>
+      <label className={className} htmlFor={inputId}>{label}</label>
+      <input disabled={disabled} id={inputId} name={name} placeholder={placeholder} required={required} type={type} value={value} onChange={onChange} />
+    </Input>
+  )
+}
+
+const Input = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  input{
+    height: 40px;
+    border: 1px solid #999;
+    border-radius: 5px;
+    padding: 0 15px;
+  }
+  input::placeholder{
+    color: #999;
+  }
+`
+
+export default FormInput;
