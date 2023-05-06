c35aa709c07effcf7454af18da5daeabc9c7ea6e
mkk <mk1210k@gmail.com>
2023-05-06 22:45:24 +0900

    [fix] rendering issues

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import axios from "axios";
-import styled from "styled-components";
+import axios from 'axios';
+import styled from 'styled-components';
 
-import FormInput from "../component/formInput";
-import FormButton from "../component/button";
-import Divider from "../component/divider";
+import FormInput from '../component/formInput';
+import FormButton from '../component/button';
+import Divider from '../component/divider';
 
 function SignIn() {
   const [email, setEmail] = useState('');
@@ -20,29 +20,29 @@ function SignIn() {
     const currentValue = e.target.value.toLowerCase();
     setEmail(currentValue);
 
-    if(!currentValue){
+    if (!currentValue) {
       setEmailErrormsg('이메일을 입력해주세요.');
-    } else if(currentValue.trim() === ''){
-      setEmailErrormsg('공백문자는 사용할 수 없습니다.');      
-    } else if(!currentValue.includes('@')){
+    } else if (currentValue.trim() === '') {
+      setEmailErrormsg('공백문자는 사용할 수 없습니다.');
+    } else if (!currentValue.includes('@')) {
       setEmailErrormsg('@는 반드시 포함되어야 합니다.');
-    } else if(currentValue.split('@').length > 2){
+    } else if (currentValue.split('@').length > 2) {
       setEmailErrormsg('@는 한번만 사용 가능합니다.');
-    } else{
+    } else {
       setEmailErrormsg('');
     }
-  }
-  
+  };
+
   const handlePasswordChange = (e) => {
     const currentValue = e.target.value;
     setPassword(currentValue);
 
-    if(!(currentValue.length >= 8)){
+    if (!(currentValue.length >= 8)) {
       setPasswordErrormsg('비밀번호는 8자 이상이어야 합니다.');
-    } else{
+    } else {
       setPasswordErrormsg('');
     }
-  }
+  };
 
   const handleSubmitLogin = (e) => {
     e.preventDefault();
@@ -50,76 +50,108 @@ function SignIn() {
     axios({
       method: 'post',
       url: 'https://www.pre-onboarding-selection-task.shop/auth/signin',
-      headers: {"Content-Type": "application/json"},
-      data: {email:email, password:password}
+      headers: { 'Content-Type': 'application/json' },
+      data: { email: email, password: password },
     })
-    .then((res) => {
-      localStorage.setItem("access_token", `${res.data.access_token}`);
-      alert(`${email.split('@')[0]} 님, 환영합니다!`);
-      navigate('/todo');
-    })
-    .catch(err=>{
-      console.log(err);
-      alert('잘못된 회원 정보입니다.');
-    })
-  }
+      .then((res) => {
+        localStorage.setItem('access_token', `${res.data.access_token}`);
+        alert(`${email.split('@')[0]} 님, 환영합니다!`);
+        navigate('/todo');
+        location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+        alert('잘못된 회원 정보입니다.');
+      });
+  };
 
   const handleGoSignup = (e) => {
     e.preventDefault();
     navigate('/signup');
-  }
+  };
 
-  useEffect(()=>{
-    if(localStorage.getItem('access_token')){
+  useEffect(() => {
+    if (localStorage.getItem('access_token')) {
       alert(`이미 로그인한 회원입니다.\nTodoList 페이지로 이동합니다.`);
       navigate('/todo');
     }
-  }, [])
+  }, []);
 
-  return(
+  return (
     <section className="container">
       <Divider />
       <h2 className="sectionTitle">로그인</h2>
       <Form>
         <fieldset>
           <legend>로그인 폼</legend>
-          <FormInput className="a11yHidden" id="loginId" label="이메일" name="email" placeholder="mkk@email.com" testid="email-input" type="eamil" value={email} onChange={handleEmailChange} />
-          <FormInput className="a11yHidden" id="loginPassword" label="비밀번호" name="password" placeholder="비밀번호" testid="password-input" type="password" value={password} onChange={handlePasswordChange} />
-        <div className="buttonBox">
-          <FormButton testid="signin-button" title="로그인" type="button" onClick={handleSubmitLogin} />
-          <FormButton testud="signup-button" title="회원가입" type="button" pointColor onClick={handleGoSignup} />
-        </div>
-        <p>{emailErrormsg}</p>
-        <p>{passwordErrormsg}</p>
+          <FormInput
+            className="a11yHidden"
+            id="loginId"
+            label="이메일"
+            name="email"
+            placeholder="mkk@email.com"
+            testid="email-input"
+            type="eamil"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <FormInput
+            className="a11yHidden"
+            id="loginPassword"
+            label="비밀번호"
+            name="password"
+            placeholder="비밀번호"
+            testid="password-input"
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <div className="buttonBox">
+            <FormButton
+              testid="signin-button"
+              title="로그인"
+              type="button"
+              onClick={handleSubmitLogin}
+            />
+            <FormButton
+              testud="signup-button"
+              title="회원가입"
+              type="button"
+              pointColor
+              onClick={handleGoSignup}
+            />
+          </div>
+          <p>{emailErrormsg}</p>
+          <p>{passwordErrormsg}</p>
         </fieldset>
       </Form>
     </section>
-  )
+  );
 }
 
 const Form = styled.form`
   width: 280px;
   margin: 0 auto;
-  
-  fieldset{    
+
+  fieldset {
     display: flex;
     flex-flow: column;
     gap: 20px;
   }
 
-  input{
+  input {
     width: 100%;
   }
-  
-  p{
+
+  p {
     text-align: center;
     color: red;
     height: 15px;
   }
 
-  .buttonBox button:last-child{
+  .buttonBox button:last-child {
     margin-top: 10px;
   }
-`
+`;
 
 export default SignIn;
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,136 +1,129 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import styled from "styled-components";
+import styled from 'styled-components';
 
-import FormInput from "../component/formInput";
-import FormButton from "../component/button";
-import Divider from "../component/divider";
-import todoApi from "../api/todo.api";
-
+import FormInput from '../component/formInput';
+import FormButton from '../component/button';
+import Divider from '../component/divider';
+import todoApi from '../api/todo.api';
 
 function Todo() {
   const navigate = useNavigate();
   const [createTodoArr, setCreateTodoArr] = useState([]);
   const [createTodo, setCreateTodo] = useState({
-    todo: "",
+    todo: '',
     isCompleted: false,
     edit: false,
   });
 
-  const inputNewItem = (e)=>{
+  const inputNewItem = (e) => {
     setCreateTodo({
       ...createTodo,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
-  }
+  };
 
-  const addTodoItem = async (e)=>{
+  const addTodoItem = async (e) => {
     e.preventDefault();
 
-    if(createTodo.todo==="") return;
+    if (createTodo.todo === '') return;
 
-    await todoApi.post("/todos",{
-      todo: createTodo.todo,
-      isCompleted: createTodo.isCompleted,
-    })
-    .then(
-      setCreateTodoArr([
-        ...createTodoArr,
-        createTodo
-      ])
-    )
-    .then(
-      setCreateTodo({
-        todo: "",
-        isCompleted: false,
-        edit: false
+    await todoApi
+      .post('/todos', {
+        todo: createTodo.todo,
+        isCompleted: createTodo.isCompleted,
       })
-    )
-    .catch(err=>console.log(err))
-    
-  }
+      .then(setCreateTodoArr([...createTodoArr, createTodo]))
+      .then(
+        setCreateTodo({
+          todo: '',
+          isCompleted: false,
+          edit: false,
+        })
+      )
+      .catch((err) => console.log(err));
+  };
 
-  const editButton = async (e, index, id)=>{
+  const editButton = async (e, index, id) => {
     e.preventDefault;
-        
-    setCreateTodoArr((prev)=>{
-      const isEdit = [...prev]
+
+    setCreateTodoArr((prev) => {
+      const isEdit = [...prev];
       isEdit[index].edit = true;
       return isEdit;
-    })
-  }
+    });
+  };
 
-  const removeButton = async (e, index, id)=>{
+  const removeButton = async (e, index, id) => {
     e.preventDefault;
 
-    await todoApi.delete(`/todos/${id}`,{
-      todo: createTodo.todo,
-      isCompleted: createTodo.isCompleted,
-    })
-    .then(res=>console.log(res))
-    .catch(err=>console.log(err))
-    setCreateTodoArr((prev)=>{
-      const removeTodo = [...prev]
+    await todoApi
+      .delete(`/todos/${id}`, {
+        todo: createTodo.todo,
+        isCompleted: createTodo.isCompleted,
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+    setCreateTodoArr((prev) => {
+      const removeTodo = [...prev];
       removeTodo.splice(index, 1);
       return removeTodo;
-    })
-  }
+    });
+  };
 
-  const cancelButton = (e, index)=>{
+  const cancelButton = (e, index) => {
     e.preventDefault;
-    setCreateTodoArr((prev)=>{
-      const isEdit = [...prev]
+    setCreateTodoArr((prev) => {
+      const isEdit = [...prev];
       isEdit[index].edit = false;
       return isEdit;
-    })
-  }
+    });
+  };
 
-  const submitButton = async (e, index, newValue, id)=>{
+  const submitButton = async (e, index, newValue, id) => {
     e.preventDefault;
-    setCreateTodoArr((prev)=>{
-      const submitEdit = [...prev]
+    setCreateTodoArr((prev) => {
+      const submitEdit = [...prev];
       submitEdit[index].edit = false;
       submitEdit[index].todo = newValue;
       return submitEdit;
-    })
-    await todoApi.put(`/todos/${id}`,{
+    });
+    await todoApi.put(`/todos/${id}`, {
       todo: createTodoArr[index].todo,
       isCompleted: createTodoArr[index].isCompleted,
-    })
-  }
+    });
+  };
 
-  const isChecked = async (index, id)=>{
-    setCreateTodoArr((prev)=>{
+  const isChecked = async (index, id) => {
+    setCreateTodoArr((prev) => {
       const CompleteItem = [...prev];
-      if(CompleteItem[index].isCompleted){
+      if (CompleteItem[index].isCompleted) {
         CompleteItem[index].isCompleted = false;
-      } else{
+      } else {
         CompleteItem[index].isCompleted = true;
       }
       return CompleteItem;
-    })
-    await todoApi.put(`/todos/${id}`,{
+    });
+    await todoApi.put(`/todos/${id}`, {
       todo: createTodoArr[index].todo,
       isCompleted: !createTodoArr[index].isCompleted,
-    })
+    });
     console.log(createTodoArr);
-  }
+  };
 
-  useEffect(()=>{
-    if(!localStorage.getItem('access_token')){
+  useEffect(() => {
+    if (!localStorage.getItem('access_token')) {
       alert(`로그인이 필요한 서비스입니다.\n로그인 페이지로 이동합니다.`);
       navigate('/signin');
     }
-
-    const getTodoItem = async () =>{
-      await todoApi.get("/todos")
-      .then(res=>setCreateTodoArr(res.data));
-    }
+    const getTodoItem = async () => {
+      await todoApi.get('/todos').then((res) => setCreateTodoArr(res.data));
+    };
     getTodoItem();
-  }, [])
+  }, []);
 
-  return(
+  return (
     <section className="container">
       <Divider />
       <h2 className="sectionTitle">Today Todo</h2>
@@ -138,67 +131,114 @@ function Todo() {
         <fieldset>
           <legend>New Todo List</legend>
           <div className="newTodo">
-            <FormInput className="a11yHidden" id="inputNewTodo" value={createTodo.todo} name="todo" placeholder="새 할일을 추가해보세요." onChange={inputNewItem} />
-            <FormButton className="addButton" title="추가" onClick={addTodoItem} type="submit" pointColor/>
+            <FormInput
+              className="a11yHidden"
+              id="inputNewTodo"
+              value={createTodo.todo}
+              name="todo"
+              placeholder="새 할일을 추가해보세요."
+              onChange={inputNewItem}
+            />
+            <FormButton
+              className="addButton"
+              title="추가"
+              onClick={addTodoItem}
+              type="submit"
+              pointColor
+            />
           </div>
-        <TodoList>
-          {
-            createTodoArr.map((arr, index, e)=>(
-              <li key={index+'번째 할일'}>
-                <FormInput checked={arr.isCompleted} className="a11yHidden" inputId="checkBox" type="checkbox" onChange={()=>isChecked(index, arr.id)} />
-                {
-                  arr.edit ?
-                    <FormInput testid="modify-input" value={arr.todo} className="a11yHidden" inputId="editInputBox" onChange={(e)=>{
-                      setCreateTodoArr(prev=>{
+          <TodoList>
+            {createTodoArr.map((arr, index, e) => (
+              <li key={index + '번째 할일'}>
+                <FormInput
+                  checked={arr.isCompleted}
+                  className="a11yHidden"
+                  inputId="checkBox"
+                  type="checkbox"
+                  onChange={() => isChecked(index, arr.id)}
+                />
+                {arr.edit ? (
+                  <FormInput
+                    testid="modify-input"
+                    value={arr.todo}
+                    className="a11yHidden"
+                    inputId="editInputBox"
+                    onChange={(e) => {
+                      setCreateTodoArr((prev) => {
                         const newEditTodo = [...prev];
-                        newEditTodo[index].todo = e.target.value
+                        newEditTodo[index].todo = e.target.value;
                         return newEditTodo;
-                      })
-                    }}/>
-                  :
+                      });
+                    }}
+                  />
+                ) : (
                   <>
-                    <span style={{
-                      color: `${arr.isCompleted?"gray":"black"}`,
-                      textDecoration: `${arr.isCompleted?"line-through":"none"}`,
-                      fontStyle: `${arr.isCompleted?"italic":"normal"}`
-                    }}>{arr.todo}</span>
+                    <span
+                      style={{
+                        color: `${arr.isCompleted ? 'gray' : 'black'}`,
+                        textDecoration: `${
+                          arr.isCompleted ? 'line-through' : 'none'
+                        }`,
+                        fontStyle: `${arr.isCompleted ? 'italic' : 'normal'}`,
+                      }}
+                    >
+                      {arr.todo}
+                    </span>
                   </>
-                }
-                {
-                  arr.edit ?
+                )}
+                {arr.edit ? (
                   <>
-                    <FormButton testid="submit-button" title="제출" type="button" onClick={()=>submitButton(e, index, arr.todo, arr.id)} />
-                    <FormButton testid="cancel-button" title="수정취소" type="button" onClick={()=>cancelButton(e, index)} />
+                    <FormButton
+                      testid="submit-button"
+                      title="제출"
+                      type="button"
+                      onClick={() => submitButton(e, index, arr.todo, arr.id)}
+                    />
+                    <FormButton
+                      testid="cancel-button"
+                      title="수정취소"
+                      type="button"
+                      onClick={() => cancelButton(e, index)}
+                    />
                   </>
-                  :
+                ) : (
                   <>
-                    <FormButton testid="modify-button" title="수정" type="button" onClick={()=>editButton(e, index)} />
-                    <FormButton testid="delete-button" title="삭제" type="button" onClick={()=>removeButton(e, index, arr.id)}/>
+                    <FormButton
+                      testid="modify-button"
+                      title="수정"
+                      type="button"
+                      onClick={() => editButton(e, index)}
+                    />
+                    <FormButton
+                      testid="delete-button"
+                      title="삭제"
+                      type="button"
+                      onClick={() => removeButton(e, index, arr.id)}
+                    />
                   </>
-                }
+                )}
               </li>
-            ))
-          }
-        </TodoList>
+            ))}
+          </TodoList>
         </fieldset>
       </Form>
     </section>
-  )
+  );
 }
 
 const Form = styled.form`
   width: 500px;
   margin: 0 auto;
 
-  .newTodo{
+  .newTodo {
     width: 500px;
     display: flex;
     gap: 20px;
 
-    .inputNewTodo{
+    .inputNewTodo {
       width: 400px;
     }
-    input{
+    input {
       width: 400px;
     }
   }
@@ -207,31 +247,32 @@ const Form = styled.form`
     width: 80px;
     height: 40px;
   }
-`
+`;
 
-const TodoList = styled.ul`  
+const TodoList = styled.ul`
   margin-top: 40px;
 
-  li{
+  li {
     display: flex;
     align-items: center;
     justify-content: space-between;
     margin-bottom: 20px;
 
-    input[type="checkbox"]{
+    input[type='checkbox'] {
       display: inline-block;
       width: 28px;
       height: 28px;
     }
 
-    #editInputBox, span{
+    #editInputBox,
+    span {
       width: 252px;
       word-wrap: break-word;
     }
-    span{
-      color: ${props=>props.dataChecked? "#000":"#333"};
+    span {
+      color: ${(props) => (props.dataChecked ? '#000' : '#333')};
     }
-  }  
-`
+  }
+`;
 
 export default Todo;
